6674b08466c1201a3438a85afb49e474034f5003
alexbatistaarantes <alexbatistaarantes@gmail.com>
2025-08-11 09:09:54 -0300

    refactor: altera local script shell

--- automation_of_sync/synchronization.sh
+++ automation_of_sync/synchronization.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cd 'path_to_repository'
-python3 main.py >> test.log
+
+PYTHON_SCRIPT=$1
+CONFIG_FILES_FOLDER=$2
+
+for file in $2/*.ini
+do
+    python3 $1 "$file" >> test.log
+done
+
--- run_syncs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-for file in ./config_files/*.ini
-do
-    ./main.py "$file"
-done
